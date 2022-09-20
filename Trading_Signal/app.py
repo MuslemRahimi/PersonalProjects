3be1c2977fa930bcb49117b049e35b66d9e43906
MuslemRahimi <moslem_rahimi@hotmail.de>
2022-09-20 12:52:21 +0200

    update

--- Trading_Signal/app.py
+++ Trading_Signal/app.py
@@ -20,16 +20,27 @@ import csv
 def get_data(symbol_list, period = '1d', start_time='2000-1-1', end_time = date.today()):
     df = pd.DataFrame()
     for symbol in tqdm(symbol_list):
+        df_temp = pd.DataFrame()
+
         tickerData = yf.Ticker(symbol)
         tickerData = tickerData.history(period = period, start = start_time, end = end_time)
 
         tickerData = tickerData.reset_index()
         for i in range(len(tickerData)):
 
-            df = df.append({'date': tickerData['Date'][i],'stock': symbol,'value': tickerData['Close'][i]},ignore_index=True)
+            df_temp = df_temp.append({'date': tickerData['Date'][i],'stock': symbol,'value': tickerData['Close'][i]},ignore_index=True)
     
-
+        #Signals
+        rsi= RSI(df_temp["value"], timeperiod=14)
+        rsi= rsi.fillna(0)
+        df_temp['RSI'] = rsi
+        
+        df = pd.concat([df,df_temp],axis=0)
+        #axis=0 is very important
+    
     df.sort_values(by='date',inplace=True)
+    df = df.reset_index()
+    print(df)
     return df
 
 
@@ -40,17 +51,9 @@ today = date.today()
 #symbol_list= [symbol for symbol in symbol_list['Symbol']]
 
 
-symbol_list = ['AAPL','NVDA','AMD']
-df = get_data(symbol_list, start_time='2015-1-1', end_time = today)
+symbol_list = ['AAPL','AMD']
+df = get_data(symbol_list, start_time='2021-1-1', end_time = today)
 
-
-#Signals
-'''
-rsi= RSI(df["value"], timeperiod=14)
-rsi= rsi.fillna(rsi.median())
-df['RSI'] = rsi
-'''
-print(df)
 
 
 # Initialise the app
@@ -67,7 +70,7 @@ def get_options(list_stocks):
 
     return dict_list
 
-print(get_options(df['stock'].unique()))
+
 app.layout = html.Div(
     children=[
         html.Div(className='row',
@@ -93,6 +96,9 @@ app.layout = html.Div(
                              children=[
                                  dcc.Graph(id='timeseries',
                                      config={'displayModeBar': True},
+                                     animate=True),
+                                  dcc.Graph(id='signals',
+                                     config={'displayModeBar': False},
                                      animate=True)
                              ])
                               ])
@@ -140,6 +146,44 @@ def update_timeseries(selected_dropdown_value):
               
 
     return figure
+
+
+
+
+@app.callback(Output('signals', 'figure'),
+              [Input('stockselector', 'value')])
+def update_signals(selected_dropdown_value):
+    ''' Draw traces of the feature 'change' based one the currently selected stocks '''
+    trace = []
+    df_sub = df
+    # Draw and append traces for each stock
+    for stock in selected_dropdown_value:
+        trace.append(go.Scatter(x=df_sub[df_sub['stock'] == stock]['date'],
+                                 y=df_sub[df_sub['stock'] == stock]['RSI'],
+                                 mode='lines',
+                                 opacity=0.7,
+                                 name=stock,
+                                 textposition='bottom center'))
+    traces = [trace]
+    data = [val for sublist in traces for val in sublist]
+    # Define Figure
+    figure = {'data': data,
+              'layout': go.Layout(
+                  colorway=["#5E0DAC", '#FF4F00', '#375CB1', '#FF7400', '#FFF400', '#FF0056'],
+                  template='plotly_dark',
+                  paper_bgcolor='rgba(0, 0, 0, 0)',
+                  plot_bgcolor='rgba(0, 0, 0, 0)',
+                  margin={'t': 50},
+                  height=250,
+                  hovermode='x',
+                  autosize=True,
+                  title={'text': '14 Period RSI', 'font': {'color': 'white'}, 'x': 0.5},
+                  yaxis={'showticklabels': False, 'range': [0, 100]},
+              ),
+              }
+
+    return figure
+
 
 
 
